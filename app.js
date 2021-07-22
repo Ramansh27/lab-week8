69728f68584ec3b410313a7e273bd719d8f282b9
Ramansh Chhabra <best_ramansh@hotmail.com>
2021-07-22 14:10:48 -0700

    Enter text as a paragraph in div

--- app.js
+++ app.js
@@ -5,16 +5,22 @@
 
 
 //step 2 select the first paragraph in the second div and add the class 'paragraph-color' so that the styles in the style sheet are applied to only that paragraph
-function changeClass(){
+/*function changeClass(){
 	const tempP = document.querySelector('.first-paragraph')
     tempP.classList.add('paragraph-color')
 }
 
 changeClass()
-
+*/
 // step 3 create a paragraph template using a template litteral
 function paragraph(phrase){
 	return `<p>${phrase}</p>`
 }
 
 // step 5 add the paragraph template you just created to the first div using innerHTML
+function addNewParagraph(temp){
+	const div = document.querySelector('#first-div')
+	div.innerHTML += temp
+}
+
+addNewParagraph(paragraph("Let's try it out"))
