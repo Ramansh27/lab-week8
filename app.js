887319f16f7216002548703c249174227fd9f04c
Ramansh Chhabra <best_ramansh@hotmail.com>
2021-07-22 14:06:44 -0700

    add function to add class: paragraph-one to .first-paragraph

--- app.js
+++ app.js
@@ -3,8 +3,16 @@
 //write functions for all of the following
 //use querySelector or querySelectorAll for all of the following, where needed.
 
+
 //step 2 select the first paragraph in the second div and add the class 'paragraph-color' so that the styles in the style sheet are applied to only that paragraph
+function changeClass(){
+	const tempP = document.querySelector('.first-paragraph')
+    tempP.classList.add('paragraph-color')
+}
+
+changeClass()
 
 // step 3 create a paragraph template using a template litteral
+
 
 // step 5 add the paragraph template you just created to the first div using innerHTML
